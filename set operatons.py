bcc0527d004598379aded66e3bcd8f8664345a46
Hitezverma12 <hitezverma12@yahoo.com>
2025-05-01 17:24:25 +1000

    Add set operations for intersection and union of two sets

---
+++ set operatons.py
@@ -0,0 +1,11 @@
+setx = {"green", "blue", }
+sety = { "yellow" }
+print("Original set elements:")
+print(setx)
+print(sety)
+print("Intersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
+print("Union of two said sets:")
+setz1 = setx.union(sety)
+print(setz1)
