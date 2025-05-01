a349d2c477bcbe25c7f14af1b2020796148ab4f1
Hitezverma12 <hitezverma12@yahoo.com>
2025-05-01 17:14:25 +1000

    set

---
+++ stes and arrays.py
@@ -0,0 +1,15 @@
+my_set = {1,2,3}
+print(my_set)
+
+my_set = {1,2,3,4,3,2}
+print(my_set) 
+
+my_set = set([1,2,3,2])
+print(my_set,"\n")
+
+num_set = set([0,1,3,4,5])
+print("Original_set:")
+print(num_set)
+num_set.pop()
+print("After removing an element from the said set:")
+print(num_set,"\n")
