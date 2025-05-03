02027ab21ab8da3a440a11452c4c4b3913af8e8b
Hitezverma12 <hitezverma12@yahoo.com>
2025-05-03 19:32:16 +1000

    Add set operations for difference and enhance array manipulation

--- set operatons.py
+++ set operatons.py
@@ -9,3 +9,15 @@ print(setz)
 print("Union of two said sets:")
 setz1 = setx.union(sety)
 print(setz1)
+
+setz2 = setx.difference(sety)
+print(setz2)
+
+setz3 = sety = setx.difference(setx)
+print(setz3)
+
+setz4 = setx.difference(sety)
+print(setz4)
+
+setz5= sety.difference(setx)
+print(setz5)
